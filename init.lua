120728738b3cd675b3d714f23ad0015f8ea9fcc0
Tom Dodson <t3dodson@gmail.com>
2025-11-24 06:37:10 -0800

    changes

--- init.lua
+++ init.lua
@@ -632,6 +632,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'nixfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
